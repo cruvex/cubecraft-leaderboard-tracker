be40c85ab9b0277e1054e045c5aaf2c3c753ee20
cruvex <cruvexx@gmail.com>
2026-03-25 17:08:09 +0100

    refactor: major refactor of types / player cache - changes for processing multiple games

--- main.ts
+++ main.ts
@@ -1,139 +1,120 @@
-import { UUID } from "uuidv7";
-import { z } from "zod@4.3.6";
+import { z } from "zod";
 
 const cubepanionBaseUrl = "https://cubepanion.ameliah.art/api/v2";
 const mojangBaseUrl = "https://api.mojang.com";
-
-let games: (typeof Game)[];
-
-const playerCache: Map<UUID, typeof PlayerProfile> = new Map();
-
-const trackedGames = ["team_eggwars"];
+const trackedGames = ["team_eggwars", "solo_skywars"];
 
 async function main() {
-  await loadGames();
+  const games = await fetchGames();
 
   for (const gameName of trackedGames) {
-    await processGame(gameName);
+    const game = games.find((g) => g.name === gameName);
+
+    if (!game) {
+      console.error(`Game not found: ${gameName}`);
+      continue;
+    }
+
+    await processGame(game);
   }
 }
 
-async function loadGames() {
-  games = await fetchGames();
-}
+async function processGame(game: Game) {
+  console.log(`Fetching leaderboard for ${game.displayName} (${game.id})`);
+  const leaderboard = await fetchGameLeaderboard(game?.name);
 
-function findGameByName(gameName: string): typeof Game | undefined {
-  return games.find((game: typeof Game) => game.name === gameName);
-}
-
-async function processGame(gameName: string) {
-  const game = findGameByName(gameName);
-
-  if (!game) {
-    console.error(`Game not found: ${gameName}`);
+  if (!leaderboard) {
+    console.log(`Leaderboard not found for ${game.displayName}`);
     return;
   }
 
-  console.log(`Fetching leaderboard for ${game.displayName} (${game.id})`);
-
-  const leaderboard = await fetchGameLeaderboard(game?.name);
   console.log(`Current leaderboard last updated: `, leaderboard.lastUpdated);
 
-  const lastSavedSnapshot = await getLastGameSnapshotTimestamp(game?.id);
+  const lastSavedSnapshot = await getLastGameSnapshotTimestamp(game.id);
   console.log(`Last saved snapshot: `, lastSavedSnapshot);
 
-  if (
-    !lastSavedSnapshot ||
-    new Date(leaderboard.lastUpdated) > lastSavedSnapshot
-  ) {
-    const igns = leaderboard.rows.map(
-      (row: typeof LeaderboardPosition) => row.player,
-    );
+  const isNewSnapshot =
+    !lastSavedSnapshot || new Date(leaderboard.lastUpdated) > lastSavedSnapshot;
 
-    await loadPlayerCache(igns);
+  if (isNewSnapshot) {
+    const igns = leaderboard.rows.map((row: LeaderboardPosition) => row.player);
 
-    if (playerCache.size != 200) {
-      console.log(`Player cache size: ${playerCache.size} (expected 200)`);
+    const uuidMap = await resolvePlayerUUIDs(igns);
+
+    if (uuidMap.size != 200) {
+      console.log(`Player cache size: ${uuidMap.size} (expected 200)`);
 
       return;
     }
 
-    await saveGameLeaderboardSnapshot(leaderboard);
+    await saveGameLeaderboardSnapshot(leaderboard, uuidMap);
     console.log("Leaderboard snapshot saved");
   } else {
     console.log("Leaderboard not updated since last snapshot");
   }
 }
 
-async function loadPlayerCache(igns: string[]) {
+async function resolvePlayerUUIDs(
+  igns: string[],
+): Promise<Map<string, string>> {
   const cachedPlayers = await getCachedPlayers(igns);
-  const uncachedPlayers = igns.filter(
-    (ign: string) => !cachedPlayers.some((p) => p.player_ign === ign),
-  );
+  const cachedIgns = new Set(cachedPlayers.map((p) => p.ign.toLowerCase()));
 
-  console.log(uncachedPlayers.length + " players not found in cache");
+  const uncachedIgns = igns.filter((ign) => !cachedIgns.has(ign.toLowerCase()));
+  console.log(`${uncachedIgns.length} players not found in DB cache`);
 
-  let unknownPlayers: (typeof PlayerProfile)[] = [];
+  let unknownPlayers: PlayerProfile[] = [];
 
-  if (uncachedPlayers.length > 0) {
-    unknownPlayers = await fetchUnknownPlayers(uncachedPlayers);
+  if (uncachedIgns.length > 0) {
+    unknownPlayers = await fetchUnknownPlayers(uncachedIgns);
+    console.log(`${unknownPlayers.length} players fetched from Mojang`);
 
-    if (!unknownPlayers || unknownPlayers.length === 0) {
-      console.log(
-        "Failed to fetch players from Mojang: " + uncachedPlayers.join(", "),
-      );
-    } else {
+    if (unknownPlayers.length > 0) {
+      console.log(`Inserting ${unknownPlayers.length} players into DB cache`);
       await insertCachedPlayers(unknownPlayers);
+    } else {
+      console.log(
+        `Failed to fetch players from Mojang: ${uncachedIgns.join(", ")}`,
+      );
     }
   }
 
-  const allPlayers = [
-    ...cachedPlayers,
-    ...unknownPlayers.map((p) => {
-      return { player_uuid: p.uuid, player_ign: p.ign };
-    }),
-  ];
-
-  allPlayers.forEach((player) => {
-    playerCache.set(player.player_ign, player);
+  const uuidMap = new Map<string, string>();
+  [...cachedPlayers, ...unknownPlayers].forEach((player) => {
+    uuidMap.set(player.ign.toLowerCase(), player.uuid);
   });
+
+  return uuidMap;
 }
 
 async function saveGameLeaderboardSnapshot(
-  leaderboard: typeof LeaderboardResponse,
-): Promise<typeof LeaderboardSnapshotRow> {
+  leaderboard: Leaderboard,
+  uuidMap: Map<string, string>,
+) {
   const row = await Bun.sql`
     INSERT INTO leaderboard_snapshots (id, game_id, timestamp)
     VALUES (${Bun.randomUUIDv7()}, ${leaderboard.gameId}, ${leaderboard.lastUpdated})
     RETURNING id, game_id, timestamp
   `;
 
-  const parsed = LeaderboardSnapshotRow.safeParse(row[0]);
-
+  const parsed = LeaderboardSnapshotRowSchema.safeParse(row[0]);
   if (!parsed.success)
     throw new Error(
       "Failed to parse created leaderboard snapshot: " + parsed.error.message,
     );
 
-  leaderboard.rows.forEach((row: typeof LeaderboardPosition) => {
-    console.log(row);
-    console.log(playerCache.get(row.player));
-  });
+  const snapshotId = parsed.data.id;
 
-  const leaderboardRows = leaderboard.rows.map(
-    (row: typeof LeaderboardPosition) => ({
-      id: Bun.randomUUIDv7(),
-      snapshot_id: parsed.data.id,
-      position: row.position,
-      player: playerCache.get(row.player).player_uuid || row.player,
-      score: row.score,
-      texture: row.texture,
-    }),
-  );
+  const leaderboardRows = leaderboard.rows.map((row) => ({
+    id: Bun.randomUUIDv7(),
+    snapshot_id: snapshotId,
+    position: row.position,
+    player: uuidMap.get(row.player.toLowerCase()) || row.player,
+    score: row.score,
+    texture: row.texture,
+  }));
 
   await Bun.sql`INSERT INTO leaderboard_rows ${Bun.sql(leaderboardRows)}`;
-
-  return parsed;
 }
 
 async function getLastGameSnapshotTimestamp(
@@ -150,20 +131,18 @@ async function getLastGameSnapshotTimestamp(
   return new Date(res[0].last_updated);
 }
 
-async function getCachedPlayers(
-  igns: string[],
-): Promise<(typeof IgnHistoryRow)[]> {
+async function getCachedPlayers(igns: string[]): Promise<PlayerProfile[]> {
   const res = await Bun.sql`
     SELECT DISTINCT ON (player_uuid)
         id,
-        player_ign,
-        player_uuid
+        player_ign AS ign,
+        player_uuid AS uuid
     FROM ign_history
     WHERE LOWER(player_ign) IN ${Bun.sql(igns.map((ign) => ign.toLowerCase()))}
     ORDER BY player_uuid, id
   `;
 
-  const parsed = z.array(IgnHistoryRow).safeParse(res);
+  const parsed = z.array(PlayerProfileShema).safeParse(res);
 
   if (!parsed.success) {
     console.error("Invalid response:", parsed.error);
@@ -173,27 +152,25 @@ async function getCachedPlayers(
   return parsed.data;
 }
 
-async function insertCachedPlayers(
-  players: (typeof PlayerProfile)[],
-): Promise<void> {
-  players = players.map((player) => ({
+async function insertCachedPlayers(players: PlayerProfile[]): Promise<void> {
+  const mappedPlayers = players.map((player) => ({
     id: Bun.randomUUIDv7(),
     player_ign: player.ign,
     player_uuid: player.uuid,
   }));
 
   await Bun.sql`
-    INSERT INTO ign_history ${Bun.sql(players)}
+    INSERT INTO ign_history ${Bun.sql(mappedPlayers)}
   `;
 }
 
 async function fetchGameLeaderboard(
   game: string,
-): Promise<typeof LeaderboardResponse> {
+): Promise<Leaderboard | undefined> {
   const res = await fetch(`${cubepanionBaseUrl}/Leaderboard/game/${game}`);
   const json = await res.json();
 
-  const parsed = LeaderboardResponse.safeParse(json);
+  const parsed = LeaderboardSchema.safeParse(json);
 
   if (!parsed.success) {
     console.error("Invalid response:", parsed.error);
@@ -203,7 +180,7 @@ async function fetchGameLeaderboard(
   return parsed.data;
 }
 
-async function fetchGames(): Promise<typeof GameResponse> {
+async function fetchGames(): Promise<Game[]> {
   const res = await fetch(`${cubepanionBaseUrl}/Games`);
   const json = await res.json();
 
@@ -211,16 +188,14 @@ async function fetchGames(): Promise<typeof GameResponse> {
 
   if (!parsed.success) {
     console.error("Invalid response:", parsed.error);
-    return;
+    return [];
   }
 
   return parsed.data;
 }
 
-async function fetchUnknownPlayers(
-  igns: string[],
-): Promise<(typeof PlayerProfile)[]> {
-  const results: (typeof PlayerProfile)[] = [];
+async function fetchUnknownPlayers(igns: string[]): Promise<PlayerProfile[]> {
+  const results: PlayerProfile[] = [];
 
   const chunkSize = 10;
   for (let i = 0; i < igns.length; i += chunkSize) {
@@ -231,9 +206,7 @@ async function fetchUnknownPlayers(
   return results;
 }
 
-async function fetchPlayerProfiles(
-  igns: string[],
-): Promise<(typeof PlayerProfile)[]> {
+async function fetchPlayerProfiles(igns: string[]): Promise<PlayerProfile[]> {
   const res = await fetch(`${mojangBaseUrl}/profiles/minecraft`, {
     method: "POST",
     body: JSON.stringify(igns),
@@ -245,7 +218,7 @@ async function fetchPlayerProfiles(
     uuid: profile.id,
   }));
 
-  const parsed = z.array(PlayerProfile).safeParse(mapped);
+  const parsed = z.array(PlayerProfileShema).safeParse(mapped);
 
   if (!parsed.success) {
     console.error("Invalid response:", parsed.error);
@@ -255,24 +228,31 @@ async function fetchPlayerProfiles(
   return parsed.data;
 }
 
-const PlayerProfile = z.object({
+const PlayerProfileShema = z.object({
+  id: z.string().optional(),
   ign: z.string(),
   uuid: z.string(),
 });
 
-const IgnHistoryRow = z.object({
+type PlayerProfile = z.infer<typeof PlayerProfileShema>;
+
+const IgnHistoryRowSchema = z.object({
   id: z.uuidv7(),
   player_ign: z.string(),
   player_uuid: z.string(),
 });
 
-const LeaderboardSnapshotRow = z.object({
+type IgnHistoryRow = z.infer<typeof IgnHistoryRowSchema>;
+
+const LeaderboardSnapshotRowSchema = z.object({
   id: z.uuidv7(),
   game_id: z.number(),
   timestamp: z.date(),
 });
 
-const LeaderboardPosition = z.object({
+type LeaderboardSnapshotRow = z.infer<typeof LeaderboardSnapshotRowSchema>;
+
+const LeaderboardPositionSchema = z.object({
   gameId: z.number(),
   position: z.number(),
   player: z.string(),
@@ -280,13 +260,17 @@ const LeaderboardPosition = z.object({
   texture: z.string(),
 });
 
-const LeaderboardResponse = z.object({
+type LeaderboardPosition = z.infer<typeof LeaderboardPositionSchema>;
+
+const LeaderboardSchema = z.object({
   gameId: z.number(),
-  lastUpdated: z.iso.datetime(),
-  rows: z.array(LeaderboardPosition),
+  lastUpdated: z.coerce.date(),
+  rows: z.array(LeaderboardPositionSchema),
 });
 
-const Game = z.object({
+type Leaderboard = z.infer<typeof LeaderboardSchema>;
+
+const GameSchema = z.object({
   id: z.number(),
   name: z.string(),
   displayName: z.string(),
@@ -297,6 +281,10 @@ const Game = z.object({
   hasPreLobby: z.boolean(),
 });
 
-const GameResponse = z.array(Game);
+type Game = z.infer<typeof GameSchema>;
 
-main();
+const GameResponse = z.array(GameSchema);
+
+await main();
+
+process.exit(0);
